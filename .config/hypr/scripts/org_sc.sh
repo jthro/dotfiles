7d7f6c3f727e9fe345d26b78e0daf5395a34c900
jthro <jethro.rosette@outlook.com>
2025-09-07 14:33:00 +1000

    Make screenshotting, org screenshotting nicer

--- .config/hypr/scripts/org_sc.sh
+++ .config/hypr/scripts/org_sc.sh
@@ -2,8 +2,13 @@
 
 save_dir="$HOME/org-roam/_media/"
 
-filename=$(wofi --dmenu --prompt "Filename for screenshot:")
+filename=$(rofi -dmenu -p "Filename for screenshot:")
 
 [ -z "$filename" ] && exit 1
 
 grim -g "$(slurp)" "$save_dir/$filename.png"
+
+link="#+attr_org: :width 400
+[[file:../../_media/$filename.png]]"
+
+echo -n "$link" | wl-copy
